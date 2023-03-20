39714a0d8ede9bead3a7fa96d90e457c469cc61a
beckystorch <user@users-MacBook-Pro-10.local>
2023-03-20 15:45:27 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,5 @@
+day_of_the_week = "Monday"
+current_week = 1
+current_day_of_the_week = 1
+
+print("Today is " + day_of_the_week + ", week " + str(current_week) + " day " + str(current_day_of_the_week) + " of codeclan.")
